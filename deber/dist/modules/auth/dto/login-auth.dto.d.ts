9f19df72818c3df3be31a00464e22337e79350ed
JoselynD26 <jmr.dicao@yavirac.edu.ec>
2024-06-14 07:41:31 -0500

    Add files via upload

---
+++ deber/dist/modules/auth/dto/login-auth.dto.d.ts
@@ -0,0 +1,5 @@
+import { User } from "../interfaces/user.interface";
+export declare class LoginAuthDto implements User {
+    email: string;
+    password: string;
+}
---
+++ deber/src/modules/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports:[
+    JwtModule.register({
+      secret: "mi codigo secreto",
+      signOptions:{expiresIn:'60s'}
+    })
+  ],
+  providers: [AuthService,JwtStrategy],
+  controllers: [AuthController]
+})
+export class AuthModule {}
